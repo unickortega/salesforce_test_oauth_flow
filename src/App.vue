b84b78cd4dbb5ceb81f14c360367be17ba7e4cda
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-17 16:21:09 +0800

    Merging updates on login page

--- src/App.vue
+++ src/App.vue
@@ -4,68 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <!-- <router-view /> -->
-    <div>
-      <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="#"><b>geechs</b> job</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04"
-          aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExample04">
-          <ul class="navbar-nav mr-auto">
-          </ul>
-          <div class="form-inline my-2 my-md-0">
-            <button class="btn btn-primary btn-sm mr-2" type="text" placeholder="Search">Start working time</button>
-            <img id="prof_img" src="../src/assets/echo.jpg" class="rounded-circle" alt="">
-          </div>
-        </div>
-      </nav>
-      <main class="py-4">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-11">
-              <div class="container-fluid">
-                <b-card no-body>
-                  <b-tabs card>
-                    <b-tab title="Top" active>
-                      <topPage />
-                    </b-tab>
-                    <b-tab title="Time Management">
-                      <!-- Tab Contents 2 -->
-                      <TimeManagement></TimeManagement>
-                    </b-tab>
-                    <b-tab title="Project Management">
-                      Tab Contents 2
-                    </b-tab>
-                    <b-tab title="Remuneration">
-                      Tab Contents 2
-                    </b-tab>
-                    <b-tab title="Questionnaire">
-                      Tab Contents 2
-                    </b-tab>
-                    <b-tab title="Chat">
-                      Tab Contents 2
-                    </b-tab>
-                  </b-tabs>
-                </b-card>
-              </div>
-            </div>
-            <div class="col-md-1">
-              <div class="float-right">
-                <b-nav vertical class="w-25">
-                  <b-nav-item active>Active</b-nav-item>
-                  <b-nav-item>Link</b-nav-item>
-                  <b-nav-item>Another Link</b-nav-item>
-                  <b-nav-item disabled>Disabled</b-nav-item>
-                </b-nav>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+    <router-view />
   </div>
 </template>
 
