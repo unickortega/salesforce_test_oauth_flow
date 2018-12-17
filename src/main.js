4af7413e21c62eb2bc7f5a429272ba240a9230b8
unickortega <uelmar_ortega@yahoo.com>
2018-12-17 15:23:20 +0800

    login Temp UI, axios, vform, scss files, auth state

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/'
 import './registerServiceWorker'
 import BootstrapVue from 'bootstrap-vue'
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
@@ -13,4 +14,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
