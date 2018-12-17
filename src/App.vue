8f3184aa72c858d8e9710ec922c89b9d80d325ef
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-17 15:51:13 +0800

    Merging updates

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <router-view/>
-  <div>
+    <!-- <router-view /> -->
+    <div>
       <nav class="navbar navbar-expand-md navbar-light bg-light">
         <a class="navbar-brand" href="#"><b>geechs</b> job</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04"
+          aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -32,7 +33,8 @@
                       <topPage />
                     </b-tab>
                     <b-tab title="Time Management">
-                      Tab Contents 2
+                      <!-- Tab Contents 2 -->
+                      <TimeManagement></TimeManagement>
                     </b-tab>
                     <b-tab title="Project Management">
                       Tab Contents 2
@@ -51,50 +53,58 @@
               </div>
             </div>
             <div class="col-md-1">
-                <div class="float-right">
-                  <b-nav vertical class="w-25">
-                    <b-nav-item active>Active</b-nav-item>
-                    <b-nav-item>Link</b-nav-item>
-                    <b-nav-item>Another Link</b-nav-item>
-                    <b-nav-item disabled>Disabled</b-nav-item>
-                  </b-nav>
-                </div>
+              <div class="float-right">
+                <b-nav vertical class="w-25">
+                  <b-nav-item active>Active</b-nav-item>
+                  <b-nav-item>Link</b-nav-item>
+                  <b-nav-item>Another Link</b-nav-item>
+                  <b-nav-item disabled>Disabled</b-nav-item>
+                </b-nav>
+              </div>
             </div>
           </div>
         </div>
       </main>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#prof_img{
-  height: 50px;
-  width: 50px;
-}
-body{
-  background-color: #f8f9fa !important;
-}
-.card{
-  border:none;
-}
-.card-header{
-  background-color: #f8f9fa;
-}
-.card-body{
-  border-right: 1px solid rgba(0, 0, 0, 0.125);
-  border-left: 1px solid rgba(0, 0, 0, 0.125);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
-}
+  #prof_img {
+    height: 50px;
+    width: 50px;
+  }
+
+  body {
+    background-color: #f8f9fa !important;
+  }
+
+  .card {
+    border: none;
+  }
+
+  .card-header {
+    background-color: #f8f9fa;
+  }
+
+  .card-body {
+    border-right: 1px solid rgba(0, 0, 0, 0.125);
+    border-left: 1px solid rgba(0, 0, 0, 0.125);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+  }
 </style>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import topPage from '../src/components/Top'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  import topPage from '../src/components/Top'
+  import TimeManagement from '../src/components/TimeManagement'
 
-export default {
-  components: {
-    topPage
-  },
-}
+
+  export default {
+    components: {
+      topPage,
+      TimeManagement
+    },
+  }
 </script>
