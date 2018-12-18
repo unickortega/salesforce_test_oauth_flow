6c59716e68699a1a1844f3902e5e2bb967ffc570
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-18 14:30:07 +0800

    Minor update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,45 +4,57 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div>
 
+      <b-navbar toggleable="md" type="light" variant="light">
 
-      <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a class="navbar-brand" href="#">Carousel</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
-            <form class="form-inline mt-2 mt-md-0">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-              <ul class="navbar-nav ml-md-auto d-md-flex">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Hasome
-                    <span class="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Top Menu Items</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
-                </li>
-              </ul>
-            </form>
-          </div>
-        </nav>
-      </header>
+        <b-navbar-brand href="#">
+          <b>geechs</b>
+          job
+        </b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+
+          <!-- <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav> -->
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+
+            <!-- <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form> -->
+            <b-nav-item href="#">
+                  <button class="btn btn-primary btn-sm mr-2" type="text" placeholder="Search">Start working time</button>
+            </b-nav-item>
+            <b-nav-item href="#">
+                  <img id="prof_img" src="../../src/assets/echo.jpg" class="rounded-circle" alt="">
+            </b-nav-item>
+
+            <!-- <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown> -->
+
+            <!-- <b-nav-item-dropdown right> -->
+              <!-- Using button-content slot -->
+              <!-- <template slot="button-content">
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Signout</b-dropdown-item>
+            </b-nav-item-dropdown> -->
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-navbar>
+
+      <!-- navbar-1.vue -->
 
 
 
